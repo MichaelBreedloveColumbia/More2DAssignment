ed9ae4a772a524441c0fa65ecff05c13bf61704e
MichaelBreedloveColumbia <112978110+MichaelBreedloveColumbia@users.noreply.github.com>
2022-09-28 17:10:17 -0500

    Submit Assignment

---
+++ More2D/More2D/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tao.FreeGlut;
+
+namespace More2D
+{
+    internal class Program
+    {
+        private static int width = 1200, height = 720;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE|Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("GAP - GLUT OpenGL Demo");
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+            Glut.glutLeaveMainLoop();
+
+            Console.ReadKey();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            Glut.glutSwapBuffers();
+        }
+    }
+}
